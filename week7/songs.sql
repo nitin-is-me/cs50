eabdfaa3d435f50cc6fdc6a4c5786dee1483b5a7
Nitin <nitinjha2609@gmail.com>
2026-07-09 12:42:47 +0530

    Create schema for songs and artists tables
    Added SQL schema for songs and artists tables.

--- week7/songs.sql
+++ week7/songs.sql
@@ -1,3 +1,24 @@
+-- Schema:
+
+-- sqlite> .schema
+CREATE TABLE songs (
+    id INTEGER,
+    name TEXT,
+    artist_id INTEGER,
+    danceability REAL,
+    energy REAL,
+    key INTEGER,
+    loudness REAL,
+    speechiness REAL,
+    valence REAL,
+    tempo REAL,
+    duration_ms INTEGER
+);
+CREATE TABLE artists (
+    id INTEGER,
+    name TEXT
+);
+
 -- write a SQL query to list the names of all songs in the database
 SELECT name FROM songs;
 
